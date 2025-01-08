ce188de4242751f1e298336818345a354240fd21
Lars Hartvigsen <lars.hartvigsen@nav.no>
2025-01-08 14:38:53 +0100

    Fix for ios

--- build_ios.sh
+++ build_ios.sh
@@ -1,2 +1,2 @@
 GOOGLE_CLIENT_ID=$(grep GOOGLE_CLIENT_ID .env | cut -d '=' -f2)
-/usr/libexec/PlistBuddy -c "Set CFBundleURLSchemes:0 $GOOGLE_CLIENT_ID" ios/Runner/Info.plist
+/usr/libexec/PlistBuddy -c "Set CFBundleURLTypes:0:CFBundleURLSchemes:0 $GOOGLE_CLIENT_ID" ios/Runner/Info.plist
